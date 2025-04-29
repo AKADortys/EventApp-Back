e073123fb2ecd9d7c5f2b6167201e1f536b0d4f3
AKADortys <thibault.a@outlook.com>
2025-04-29 16:46:57 +0200

    Début service pour registration

--- src/services/registration.service.js
+++ src/services/registration.service.js
@@ -1,3 +1,56 @@
-const RegistrationModel = require("../models/Registration");
+const Registration = require("../models/Registration");
+const validator = require("validator");
 
-module.exports = {};
+module.exports = {
+  getRegistrations: async (page, limit, search) => {
+    try {
+      const skip = (page - 1) * limit;
+      if (search && !validator.isLength(search, { min: 3, max: 30 })) {
+        throw new Error(
+          "La recherche est invalide (doit faire entre 3 et 30 caractères)"
+        );
+      }
+      const searchQuery = search
+        ? {
+            $or: [
+              { event: { $regex: searchQuery, $options: "i" } },
+              { user: { $regex: searchQuery, $options: "i" } },
+            ],
+          }
+        : {};
+      // Requête avec filtre + pagination
+      const [registration, total] = await Promise.all([
+        Registration.find(searchQuery)
+          .skip(skip)
+          .limit(limit)
+          .populate("organizer", "name lastName mail")
+          .populate("participants", "name lastName mail"),
+        Registration.countDocuments(searchQuery),
+      ]);
+      return {
+        registration,
+        total,
+        totalPages: Math.ceil(total / limit),
+        page,
+      };
+    } catch (error) {
+      console.error(
+        "Erreur registration.services getRegistrations()\n" + error
+      );
+      throw new Error("Erreur lors de la récupération des inscriptions");
+    }
+  },
+
+  getRegistration: async (id) => {
+    try {
+      const registration = await Registration.findById(id)
+        .populate("organizer", "name lastName mail")
+        .populate("participants", "name lastName mail");
+
+      return registration || null;
+    } catch (error) {
+      console.error("Erreur registration.services getRegistration()\n" + error);
+      throw new Error("Erreur lors de la récupération de l'event");
+    }
+  },
+};
